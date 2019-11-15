5e44b453778f475c3e7f7d6df783642b934b2463
hyeyeon <ginahy0210@gmail.com>
2019-11-15 16:27:50 +0900

    png file to test

--- testFile/RSA_test_usingServer_HY.py
+++ testFile/RSA_test_usingServer_HY.py
@@ -3,8 +3,10 @@ from Crypto.Cipher import PKCS1_OAEP
 
 #개인키 읽어와서 return
 def ReadPEM():
-    h = open("","r") #파일로 저장했던 개인키 불러오기 PEM 파일 경로로 넣기
-    privateKey = RSA.import_key(h.head())
+    # 파일로 저장했던 개인키 불러오기 PEM 파일 경로로 넣기
+    h = open("C:\\Users\\ginah\\PycharmProjects\\ransomware\\original file\\private.pem","r")
+    #passpharse는 cmd에서 키를 만들때 사용한 pass이다. #여기에 이게 들어가도 되는지는 잘 모르겠음..
+    privateKey = RSA.import_key(h.read(), passphrase="antdd")
     h.close();
     return privateKey;
 
@@ -31,4 +33,5 @@ if __name__ == '__main__':
     DECtext = rsa_dec(ENCtext)
 
     print(ENCtext)
+    print("")
     print(DECtext)
